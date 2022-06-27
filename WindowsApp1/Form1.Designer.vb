b5c9c3a689b0a7469086f1ad9f0e1089671c2a15
RohitPasi07 <rohitpassi245@gmail.com>
2022-06-27 14:27:42 +0530

    Initial commit

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Write = New System.Windows.Forms.Button()
+        Me.TextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Write
+        '
+        Me.Write.Location = New System.Drawing.Point(304, 364)
+        Me.Write.Name = "Write"
+        Me.Write.Size = New System.Drawing.Size(140, 35)
+        Me.Write.TabIndex = 1
+        Me.Write.Text = "Write"
+        Me.Write.UseVisualStyleBackColor = True
+        '
+        'TextBox
+        '
+        Me.TextBox.Location = New System.Drawing.Point(96, 74)
+        Me.TextBox.Multiline = True
+        Me.TextBox.Name = "TextBox"
+        Me.TextBox.Size = New System.Drawing.Size(589, 268)
+        Me.TextBox.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox)
+        Me.Controls.Add(Me.Write)
+        Me.Name = "Form1"
+        Me.Text = "Hello"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Write As Button
+    Friend WithEvents TextBox As TextBox
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.IO
+Public Class Form1
+
+
+    Dim status As Boolean
+  
+
+
+    Public Sub Input(ByRef msg As String, ByRef Status As Boolean)
+        Dim strFile As String = "D:\WritetoErrLog_" & DateTime.Today.ToString("dd-MMM-yyyy") & ".txt"
+        Dim sw As StreamWriter
+        Dim fs As FileStream
+        Try
+            If (Not File.Exists(strFile)) Then
+                Try
+                    fs = File.Create(strFile)
+                    sw = File.AppendText(strFile)
+                    sw.WriteLine("Start Error Log for today")
+                    Status = True
+
+
+                Catch ex As Exception
+                    MsgBox("Error Creating Log File")
+                    Status = False
+
+                End Try
+            Else
+                sw = File.AppendText(strFile)
+                sw.WriteLine(msg & " " & DateTime.Now)
+                Status = True
+                sw.Close()
+            End If
+
+
+        Catch ex As Exception
+            Status = False
+
+            MsgBox(Status)
+
+        End Try
+
+    End Sub
+
+
+    Private Sub Write_Click(sender As Object, e As EventArgs) Handles Write.Click
+        status = True
+        Input(TextBox.Text, status)
+
+    End Sub
+
+
+End Class
+
+
